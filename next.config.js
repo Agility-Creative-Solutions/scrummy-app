f568a33e76aa152b8c5caf0830f143897a4c03ef
Pedro Lima <pedrohblima03@gmail.com>
2022-07-16 15:17:11 -0300

    fix: convert export at bundle-analyzer to ES module

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
 });
 
-module.exports = withBundleAnalyzer({
-  distDir: 'build',
-});
+export default withBundleAnalyzer({});
