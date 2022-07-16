f3e14667e9112b4814bb79364bf647205ef7405e
Pedro Lima <pedrohblima03@gmail.com>
2022-07-16 16:00:32 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
 });
 
-module.exports = withBundleAnalyzer({
-  eslint: {
-    dirs: ['.'],
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  reactStrictMode: true,
-});
+module.exports = (phase, defaultConfig) => {
+  return withBundleAnalyzer(defaultConfig);
+};
