31e9881e93be9fd3e41cb7b1956a92a8bdc75ead
Renan Martins <renanpontez@gmail.com>
2022-08-04 09:58:40 -0300

    feat: create design system page and install styled-components

---
+++ src/pages/design-system.tsx
@@ -0,0 +1,9 @@
+import Input from '@/components/atoms/Input';
+
+const DesignSystem = () => (
+  <div style={{ backgroundColor: '#e3e3e3', height: '100vh', padding: 20 }}>
+    <Input type="text" />
+  </div>
+);
+
+export default DesignSystem;
