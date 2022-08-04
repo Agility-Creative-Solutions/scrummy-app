e8465edeb51ce5afd468d28c4a74bcde99af4b13
Renan Martins <renanpontez@gmail.com>
2022-08-04 10:04:01 -0300

    feat: add button

--- src/components/atoms/Input/bin/InputStyled.ts
+++ src/components/atoms/Input/bin/InputStyled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export default styled.input`
-  background-color: red;
+  background-color: white;
 `;
--- src/pages/design-system.tsx
+++ src/pages/design-system.tsx
@@ -1,8 +1,18 @@
+import Button from '@/components/atoms/Button';
 import Input from '@/components/atoms/Input';
 
 const DesignSystem = () => (
   <div style={{ backgroundColor: '#e3e3e3', height: '100vh', padding: 20 }}>
-    <Input type="text" />
+    <div className="grid gap-y-5">
+      <div>
+        <Input type="text" />
+        <Input type="text" />
+        <Input type="text" />
+      </div>
+      <div>
+        <Button title="Test Button" buttonType="primary" />
+      </div>
+    </div>
   </div>
 );
 
