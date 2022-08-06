3b25c90ff00b92a74eba365a7e791cf38ecdb7b7
Pedro Lima <pedrohblima03@gmail.com>
2022-08-05 23:43:46 -0300

    fix: button and input styles

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import React from 'react';
 
 import useMdIcons from '@/hooks/useMdIcons';
@@ -24,17 +25,22 @@ const Input: React.FC<InputProps> = ({
   name,
   value,
 }) => {
-  const { Icon } = useMdIcons(icon ?? '');
+  const { Icon } = useMdIcons(icon ?? 'MdInput');
 
   return (
-    <div className="relative">
+    <div className="relative m-0 my-5 rounded-lg bg-gray-100 px-5">
       {icon && (
-        <span className="absolute left-0 flex h-full items-center justify-center">
-          {<Icon />}
+        <span className="absolute left-0 top-0 flex h-full items-center justify-center pl-5">
+          {<Icon className="text-lg text-gray-500" color="inherit" />}
         </span>
       )}
       <input
-        className="w-full rounded-lg bg-gray-100 px-4 py-2 text-gray-700 focus:outline-none"
+        className={cx(
+          'w-full rounded-lg bg-gray-100 text-gray-700 focus:outline-none py-2',
+          {
+            'pl-8': icon,
+          }
+        )}
         name={name}
         type={type}
         placeholder={placeholder && !floatingPlaceholder ? placeholder : ''}
--- src/pages/design-system.tsx
+++ src/pages/design-system.tsx
@@ -4,12 +4,24 @@ const DesignSystem = () => (
   <div style={{ backgroundColor: '#e3e3e3', height: '100vh', padding: 20 }}>
     <div className="grid gap-y-5">
       <div>
-        <Input type="text" onChange={() => console.log('Lorem')} />
+        <Input
+          type="text"
+          icon="MdInput"
+          onChange={() => console.log('Lorem')}
+        />
         <Input type="text" onChange={() => console.log('Lorem')} />
         <Input type="text" onChange={() => console.log('Lorem')} />
       </div>
-      <div>
+      <div className="flex-column flex items-center justify-center gap-5">
         <Button title="Test Button" buttonType="primary" />
+        <Button icon="MdInput" buttonType="primary" />
+        <Button title="Test Button" buttonType="secondary" />
+        <Button
+          title="Test Button"
+          icon="MdInput"
+          buttonType="tertiary"
+          fullWidth
+        />
       </div>
     </div>
   </div>
