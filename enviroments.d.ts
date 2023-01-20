8542dd4854e35c542ceaab99d46fdbc107fe3089
Pedro Lima <pedrohblima03@gmail.com>
2023-01-19 23:22:57 -0300

    feat: remove unecessary enviroment in d.ts file

--- enviroments.d.ts
+++ enviroments.d.ts
@@ -1,7 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     readonly NEXT_PUBLIC_API_URL: string;
-    readonly NEXT_PUBLIC_LOCAL_STORAGE_PREFIX: string;
     readonly NEXT_PUBLIC_LOCAL_STORAGE_USER_KEY: string;
     readonly NEXT_PUBLIC_LOCAL_STORAGE_TOKENS_KEY: string;
   }
