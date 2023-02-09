e8a06349617b4aa105f7a3efd204c8451d6f88e5
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-02-09 09:43:12 -0300

    feat: register page with api

--- src/service/auth/service.ts
+++ src/service/auth/service.ts
@@ -1,9 +1,13 @@
-import type { LogInType } from '../../types/auth/login';
+import type { LogInType, RegisterAccountType } from '../../types/auth/login';
 import http from '../http.service';
 
 const UserService = {
   login: async (data: LogInType) => {
     const response = await http.POST('/auth/login', data);
+    return response;
+  },
+  register: async (data: RegisterAccountType) => {
+    const response = await http.POST('/auth/register', data);
     return response;
   },
 };
